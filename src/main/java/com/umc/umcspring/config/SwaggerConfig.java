ed9de6a8c79762117c8fbee35564dacdf458f9f0
Suanna01 <yangnony01@gmail.com>
2023-05-23 11:12:51 +0900

    #1 feat : Swagger 설정

--- src/main/java/com/umc/umcspring/config/SwaggerConfig.java
+++ src/main/java/com/umc/umcspring/config/SwaggerConfig.java
@@ -3,20 +3,32 @@ package com.umc.umcspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("UMC-Spring")
+                .version("0.0.1")
+                .description("API")
+                .build();
+    }
     @Bean
     public Docket swagger() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("umc")
+                .apiInfo(this.apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.umc.umcspring"))
                 .paths(PathSelectors.any())
                 .build();
     }
