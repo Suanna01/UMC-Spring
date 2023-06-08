49ad84ae688922bf9f49541e1220a51ba8920a21
Suanna01 <yangnony01@gmail.com>
2023-06-08 16:12:15 +0900

    #2 feat : validation 추가

--- src/main/java/com/umc/umcspring/ResponseMessage.java
+++ src/main/java/com/umc/umcspring/ResponseMessage.java
@@ -5,6 +5,7 @@ public class ResponseMessage {
     public static final String CREATE_USER_FAIL = "회원 가입 실패";
     public static final String READ_USER = "회원 정보 조회 성공";
     public static final String UPDATE_USER = "회원 정보 수정 성공";
+    public static final String UPDATE_USER_FAIL = "회원 정보 수정 실패";
     public static final String DELETE_USER = "회원 탈퇴 성공";
     public static final String READ_USER_PROFILE = "회원 프로필 조회 성공";
 
@@ -14,4 +15,5 @@ public class ResponseMessage {
     public static final String PATCH_PROD_PRICE = "상품 가격 할인 성공";
     public static final String DELETE_PROD = "상품 삭제 성공";
     public static final String READ_PROD_BY_ID = "특정 상품 조회 성공";
+    public static final String READ_USER_PROFILE_FAIL = "특정 상품 조회 실패";
 }
